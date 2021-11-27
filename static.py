52eb2645b29e4306bfb439f934fcb7569fca6fc3
xsakurait <60243830+xsakurait@users.noreply.github.com>
2021-11-27 17:28:53 +0900

    Add files via upload

--- static.py
+++ static.py
@@ -5,3 +5,35 @@ static変数（クラス変数）
 static変数は、1箇所にまとめられて値を保持する（全てのオブジェクトに対して同じ値が使用される）。
 
 インスタンス変数がインスタンス毎の変数であり、static変数（クラス変数）はクラス毎の変数となります。
+class Sample {
+  int number = 99; // インスタンス変数
+  static String color = "黄"; // static変数（クラス変数）
+
+  // static変数（クラス変数）に値をセットするメソッド
+  public void setColor(String value) {
+    color = value;
+  }
+}
+
+
+class Main {
+    public static void main(String args[]) {
+        Sample sample1 = new Sample();
+
+        sample1.number = 10;
+        System.out.println(sample1.number); // 10
+        sample1.setColor("赤");
+        System.out.println(Sample.color); // 赤
+
+        Sample sample2 = new Sample();
+        // インスタンス変数であるnumberは、オブジェクト毎に保持しているものなので、初期値の99が入っている。
+        System.out.println(sample2.number); // 99
+
+        // static変数（クラス変数)のcolorは共通の値を共有するため、
+    // デフォルトの「黄」はsample1.setColorのタイミングで赤がセットされた。
+    // だからここでstatic変数（クラス変数）にアクセスすると赤と出力される。
+    System.out.println(Sample.color); // 赤
+}
+
+
+}
